5ad15e3416717d755dfb95ac0f1480814b0071f1
Pablo Zavala <juanparamosae@gmail.com>
2024-06-25 12:09:31 -0600

    CRUD usuario base de datos

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,28 +1,111 @@
 const { response } = require("express");
+const User = require("../models/user");
 
 const userGet = async (req, res = response) => {
+  try {
 
-  res.json({
-    ok: true,
-    msg: "GET Users Controller",
-  })
+    const { limite = 15, desde = 0 } = req.query;
+
+    const [total, users] = await Promise.all([
+      User.countDocuments(),
+      User.find()
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ])
+
+    res.json({
+      ok: true,
+      total,
+      users
+    });
+
+
+  } catch (error) {
+
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      errorMsg: 'Contacte al administrador',
+      error
+    });
+  }
 
 }
 
 const userGetByID = async (req, res) => {
-  const id = req.params.id
-  res.status(200).json({ ok: true, id, msg: "GET by Id Controller" })
+
+  try {
+
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    return res.json({
+      ok: true,
+      msg: 'put vito controlador',
+      user
+    });
+
+  } catch (error) {
+
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      errorMsg: 'Contacte al administrador',
+      error
+    });
+  }
+
 }
 
 const userPost = async (req, res) => {
-  const body = req.body
-  res.status(200).json({ ok: true, body, msg: "POST User Controller" })
+  try {
+    const { fullName, userName, password, role, email } = req.body;
+    const user = new User({ fullName, userName, password, email });
+
+
+    await user.save();
+
+    res.json({
+      ok: true,
+      msg: 'post InnovaTuve controlador',
+      user
+    });
+
+
+  } catch (error) {
+
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      errorMsg: 'Contacte al administrador',
+      error
+    });
+  }
 }
 
 const userPut = async (req, res) => {
-  const id = req.params.id
-  const body = req.body
-  res.status(200).json({ ok: true, id, body, msg: "PUT user Controller" })
+  try {
+
+    const { id } = req.params;
+    const { _id, ...rest } = req.body;
+    const user = await User.findByIdAndUpdate(id, rest);
+
+    res.json({
+      ok: true,
+      msg: 'put InnovaTuve controlador',
+      user
+    });
+
+  } catch (error) {
+
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      errorMsg: 'Contacte al administrador',
+      error
+    });
+
+  }
 }
 
 const userDelete = async (req, res) => {
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema({
+  fullName: {
+    type: String,
+    required: [true, 'El nombre es obligatorio']
+  },
+  userName: {
+    type: String,
+    required: [true, 'El nombre de usuario es obligatorio']
+  },
+  email: {
+    type: String,
+    required: [true, 'El correo es obligatorio']
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria']
+  },
+  favorites: {
+    type: [String],
+    default: []
+  }
+
+})
+
+userSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+
+  user.uid = _id;
+  return user;
+}
+module.exports = model('User', userSchema)
