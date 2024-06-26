00ce49d6661cfacad6d4396b8b53738c5109b25b
Pablo Zavala <juanparamosae@gmail.com>
2024-06-25 19:26:49 -0600

    validaciones de inicio de sesion

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
 const { login } = require('../controllers/auth.controller');
+const { fieldsValidation } = require('../middlewares/fieldsValidation');
+const { check } = require('express-validator');
 
 const router = Router();
 
-router.post('/', login)
+router.post('/', [
+  check('userName', 'Nombre de usuario o correo obligatorio').not().isEmpty(),
+  check('password', 'Contraseña obligatoria').not().isEmpty(),
+  fieldsValidation,
+], login)
 
 
 module.exports = router
